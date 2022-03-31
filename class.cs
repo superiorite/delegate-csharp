ca7af9c95bdf2af2f8be627c3de0691d192f49d9
superiorite <56066154+superiorite@users.noreply.github.com>
2022-03-31 13:26:07 +0300

    create class StockExchangeMonitor

---
+++ class.cs
@@ -0,0 +1,21 @@
+namespace MyLib
+{
+    public class StockExchangeMonitor
+    {
+        public delegate void PriceChange(int price);
+
+        public PriceChange PriceChangeHandler { get; set; }
+
+        public void Start()
+        {
+            while(true)
+            {
+                int bankOfAmericaPrice = (new Random()).Next(100);
+
+                PriceChangeHandler(bankOfAmericaPrice);
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
